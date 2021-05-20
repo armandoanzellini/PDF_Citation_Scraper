5689ab7fc4c022ebaec7a7d92ca2a45b36b51258
armandoanzellini <armandoanzellini@gmail.com>
2021-05-20 19:07:42 -0400

    Update PDFScraper.py
    - Fixed bugs of not finding certain references and errors reported by H. Horbaly

--- PDFScraper.py
+++ PDFScraper.py
@@ -25,9 +25,11 @@ simult   = st.checkbox('Check box if keywords and authors should be searched in 
 # Ask user to upload a file
 uploaded_files = st.file_uploader("Upload single or multiple PDFs...", type="pdf", accept_multiple_files=True)
 
-# direct = 'C:\\Users\\Armando\\Desktop\\'
+# direct = 'C:\\Users\\Armando\\Downloads\\'
 
-# file = open(direct + 'Trotter 1954 Preliminary Study of Estimation of Weight.pdf', 'rb')
+# file = open(direct + 'Trotter-Gleser_1958.pdf', 'rb')
+
+# maybe missing one of the citations!!!!!
 
 # refs     = 'Trotter and Gleser;1952'
 # keywords = 'sexual dimorphism'
@@ -85,11 +87,21 @@ class pdf_scraper(object):
             if len(j) == 3:
                 co_auth1 = f'{j[0]} and {j[1]}'
                 co_auth2 = f'{j[0]}, {j[1]}'
-                intext_refs += [[co_auth1, j[-1]], [co_auth2, j[-1]]]
+                co_auth3 = f'{j[0]} & {j[1]}'
+                intext_refs += [[co_auth1, j[-1]],
+                                [co_auth2, j[-1]],
+                                [co_auth3, j[-1]]]
             if len(j) == 4:
                 aaa_style1 = f'{j[0]}, {j[1]}, and {j[2]}'
-                aaa_style2 = f'{j[0]}, {j[1]}, {j[2]}'
-                intext_refs += [[aaa_style1, j[-1]],[aaa_style2, j[-1]]]
+                aaa_style2 = f'{j[0]}, {j[1]} and {j[2]}'
+                aaa_style3 = f'{j[0]}, {j[1]}, {j[2]}'
+                aaa_style4 = f'{j[0]}, {j[1]}, & {j[2]}'
+                aaa_style5 = f'{j[0]}, {j[1]} & {j[2]}'
+                intext_refs += [[aaa_style1, j[-1]],
+                                [aaa_style2, j[-1]],
+                                [aaa_style3, j[-1]],
+                                [aaa_style4, j[-1]],
+                                [aaa_style5, j[-1]]]
                 
         #split the keywords and clean up for searching
         keywords = keywords.split(',')
@@ -118,18 +130,22 @@ class pdf_scraper(object):
                             return i,j
                         elif 'literature cited' in str(k).casefold():
                             return i,j
+                        elif 'literature c1 ted' in str(k).casefold():
+                            return i,j
                 for j in range(len(txt[i][1])):
                     for k in txt[i][1][j]:
                         if 'acknowledgments' in str(k).casefold():
                             return i,j
                         elif 'acknowledgment' in str(k).casefold():
                             return i,j
-                        
-        pg, par = workscited(text)
+        try:                
+            pg, par = workscited(text)
+        except:
+            pg, par = -1, -1
         
         # Create lists of works cited paragraphs and regular paragraphs
         works_cited = []
-        paras  = []
+        orphan  = []
         last_page   = []
         for pag in range(len(text)):
             if pag == pg:
@@ -142,50 +158,137 @@ class pdf_scraper(object):
                 works_cited += [i[4] for i in text[pag][1]]
             else:
                 for p in range(len(text[pag][1])):
-                    paras +=[[text[pag][0],
+                    orphan +=[[text[pag][0],
                                    text[pag][1][p][4].replace('\n', ' ').replace('- ', '')]]
         
-        paras += last_page
+        orphan += last_page
         
         # retrieve abstract in case needed later
         try:
-            abstract = [i for pg, i in paras if i.startswith('ABSTRACT ')][0]
+            abstract = [i for pg, i in orphan if i.startswith('ABSTRACT ')][0]
         
             if not abstract.endswith('. '):
-                ix = paras.index([1,abstract])
-                abstract = abstract + paras[ix+1][1]
+                ix = orphan.index([1,abstract])
+                abstract = abstract + orphan[ix+1][1]
         except:
             abstract = []
         
         # make sure the abstract is not orphaned!!
         
         # Remove all paragraphs that are entirely in upper case (headers)
-        paras = [i for i in paras if not i[1].isupper()]
+        orphan = [i for i in orphan if not i[1].isupper()]
         
         # Remove any paragraph beginning with Fig and image
-        paras = [i for i in paras if not i[1].startswith('Fig')]
-        paras = [i for i in paras if not i[1].startswith('<image')]
+        orphan = [i for i in orphan if not i[1].startswith('Fig')]
+        orphan = [i for i in orphan if not i[1].startswith('<image')]
         
         # Remove any paragraph that is just affiliation or information or abstract
-        paras = [i for i in paras if not i[1].startswith('Grant sponsor:')]
-        paras = [i for i in paras if not i[1].startswith('*Correspondence')]
-        paras = [i for i in paras if not i[1].startswith('* Corresponding author')]
-        paras = [i for i in paras if not i[1].startswith('Received')]
-        paras = [i for i in paras if not i[1].startswith('DOI:')]
-        paras = [i for i in paras if not i[1].startswith('DOI ')]
-        paras = [i for i in paras if not i[1].startswith('TABLE')]
-        paras = [i for i in paras if not i[1].startswith('Keywords: ')]
-        paras = [i for i in paras if not i[1].startswith('KEY WORDS ')]
-        paras = [i for i in paras if not i[1].startswith('ABSTRACT ')]
-        paras = [i for i in paras if not i[1].startswith('Article history: ')]
+        orphan = [i for i in orphan if not i[1].startswith('Grant sponsor:')]
+        orphan = [i for i in orphan if not i[1].startswith('*Correspondence')]
+        orphan = [i for i in orphan if not i[1].startswith('* Corresponding author')]
+        orphan = [i for i in orphan if not i[1].startswith('Received')]
+        orphan = [i for i in orphan if not i[1].startswith('DOI:')]
+        orphan = [i for i in orphan if not i[1].startswith('DOI ')]
+        orphan = [i for i in orphan if not i[1].startswith('TABLE')]
+        orphan = [i for i in orphan if not i[1].startswith('Keywords: ')]
+        orphan = [i for i in orphan if not i[1].startswith('KEY WORDS ')]
+        orphan = [i for i in orphan if not i[1].startswith('ABSTRACT ')]
+        orphan = [i for i in orphan if not i[1].startswith('Article history: ')]
         
-        paras = [i for i in paras if not i[1].endswith(', Inc. ')]
+        orphan = [i for i in orphan if not i[1].endswith(', Inc. ')]
         
-        paras = [i for i in paras if not ' All rights reserved. ' in i[1]] # ending with this is also how you find the abstract in some journals
-        paras = [i for i in paras if not '@' in i[1]]
-        paras = [i for i in paras if not ' www.'  in i[1]]
-        paras = [i for i in paras if not ' w ww.' in i[1]]
+        orphan = [i for i in orphan if not ' All rights reserved. ' in i[1]] # ending with this is also how you find the abstract in some journals
+        orphan = [i for i in orphan if not '@' in i[1]]
+        orphan = [i for i in orphan if not ' www.'  in i[1]]
+        orphan = [i for i in orphan if not ' w ww.' in i[1]]
         
+        # find and remove duplicated paragraphs since they're usually metadata
+        # use length to reduce number of possible duplicates
+        non_dups = [orphan[0][1][:-5]] # have to do -5 to remove page numbers from duplicates
+        dups     = []
+        for ix in range(1, len(orphan)):
+            if orphan[ix][1][:-5] in non_dups:
+                dups += [ix]
+            elif orphan[ix][1] not in non_dups:
+                non_dups += [orphan[ix][1][:-5]]
+        
+        # find the first instance of the duplication in order to remove all dups
+        if dups:
+            for ix in range(len(orphan)):
+                if orphan[ix][1][:-5] == orphan[dups[0]][1][:-5] and ix not in dups:
+                    dups += [ix]
+                
+        # sort dups index list to prepare to drop the items in the orphan list
+        # reverse to avoid changing index while deleting
+        dups.sort(reverse=True)
+        
+        # go through orphan and remove by index
+        for i in dups:
+            del orphan[i]  
+        
+        # Orphaning fix
+        # create list without page numbers
+        unorphan = [i[1].strip() for i in orphan]
+        
+        # stop the orphaning by looking for uppercase starts and period ends
+        def end_condition(string):
+            skip = ['Dr.',
+                    'U.S.',
+                    'pp.']
+            return string[-1] == '.' and string[-3:] not in skip
+        
+        def start_condition(string):
+            return string[0].isupper()
+        
+        start_ixs = [i for i, ele in enumerate(unorphan) if start_condition(ele)]
+        #end_ixs   = [i for i, ele in enumerate(unorphan) if end_condition(ele)  ]
+        
+        start_ixs += [len(unorphan)]
+        
+        '''
+        start_list = []
+        for k, g in groupby(enumerate(start_ixs), lambda ix : ix[0] - ix[1]):
+            start_list += [list(map(itemgetter(1), g))]
+
+        end_list = []
+        for k, g in groupby(enumerate(end_ixs), lambda ix : ix[0] - ix[1]):
+            end_list += [list(map(itemgetter(1), g))]
+        
+        
+        ranges = []
+        for j in end_ixs:
+            while j not in start_ixs:
+                j -=1
+            if j in start_ixs:
+                print(j)
+                ranges += [j]
+        
+        rangelist = list(zip(ranges, end_ixs))
+        
+        missing   = []
+        for i in range(len(unorphan)):
+            if not any(i in x for x in rangelist):
+                print(i)
+        '''
+        capit_sent = list(zip(start_ixs, start_ixs[1:]))
+        
+        ps=[]
+        for i in capit_sent:
+            ps += [' '.join(unorphan[i[0]:i[1]])]
+        
+        
+        # to deal with bad OCR, anytime a word in the list below is followed
+        # by a period, the period is removed
+        ps = [i.replace(' or.', ' &') for i in ps]
+        ps = [i.replace(' et.', ' &') for i in ps]
+        
+        # Get page numbers back
+        paras = []
+        for i in ps:
+            for j in orphan:
+                if i[:30] in j[1]:
+                    paras += [[j[0], i]]
+       
         # Remove paragraphs that are mostly digits since they represent a table
         for i in paras:
             numcount = sum(c.isdigit() for c in i[1])
@@ -199,33 +302,6 @@ class pdf_scraper(object):
             letcount = sum(c.isalpha() for c in i[1])
             if numcount > letcount:
                 paras.remove(i)      
-
-        # find and remove duplicated paragraphs since they're usually metadata
-        # use length to reduce number of possible duplicates
-        non_dups = [paras[0][1][:-5]] # have to do -5 to remove page numbers from duplicates
-        dups     = []
-        for ix in range(1, len(paras)):
-            if paras[ix][1][:-5] in non_dups:
-                dups += [ix]
-            elif paras[ix][1] not in non_dups:
-                non_dups += [paras[ix][1][:-5]]
-        
-        # find the first instance of the duplication in order to remove all dups
-        if dups:
-            for ix in range(len(paras)):
-                if paras[ix][1][:-5] == paras[dups[0]][1][:-5] and ix not in dups:
-                    dups += [ix]
-                
-        # sort dups index list to prepare to drop the items in the paras list
-        # reverse to avoid changing index while deleting
-        dups.sort(reverse=True)
-        
-        # go through paras and remove by index
-        for i in dups:
-            del paras[i]
-        
-        # Delete all sections shorter than 61 characters since likely not part of paragraph
-        paras = [i for i in paras if len(i[1]) > 61]
         
         # delete all double spaces in each paragraph
         paras = [[i[0], i[1].replace('  ', ' ')] for i in paras]
@@ -312,58 +388,19 @@ class pdf_scraper(object):
         self.range_cite = range_cite                
         self.cite_nums = cite_nums
         
-        """
-        # Make sure paragraphs are not orphaned
-        orphan = []
-        for ix in reversed(range(1, len(paras))):
-            text = ''
-            j = ix
-            while paras[j][1][0].islower():
-                text = paras[ix-1][1] + paras[ix][1] + text
-                j -= 1
-                print(text)
-            orphan = [[paras[j][0], text]] + orphan
-            
-    
+ 
 
-        orphan = []
-        for ix in range(3, len(paras)):
-            page = paras[ix][0]
-            text = paras[ix][1]
-            while text[-2] != '.':
-                text += paras[ix + 1][1]
-                ix += 1
-            while text[0].islower() == True:
-                text = paras[ix - 1][1] + text
-                ix-= 1
-            orphan += [[page, text]]
-            
-        # find duplicates in orphan to reduce size of list
-        # using the same proceedure as above
-        non_dups = [orphan[0][1]] 
-        dups     = []
-        for ix in range(1, len(orphan)):
-            if orphan[ix][1] in non_dups:
-                dups += [ix]
-            elif orphan[ix][1] not in non_dups:
-                non_dups += [orphan[ix][1]]
-                
-        # sort dups index list to prepare to drop the items in the paras list
-        # reverse to avoid changing index while deleting
-        dups.sort(reverse=True)
-        
-        # go through paras and remove by index
-        for i in dups:
-            del orphan[i]
-        
-        # Orphan is still orphaning some paragraphs leading to duplications not being caught by the dups search
-    
-        """
-        
+        '''
+        Make list of just text
+        use first few words of paragraph to find which page the paragrpah starts in
+        use few words preceding citations (once found) to check if paragraph pg number different from citation
+        if so, print notye that citation in following page
+        '''
+
         return paras
             
     def find_citations(self, paras):
-        
+
         # defining regex function to find sentences with in-text citations
         def intext_match(paragraph, ref):
             pattern = rf'({ref[0]}[^A-Za-z]+{ref[1]})'
@@ -380,6 +417,20 @@ class pdf_scraper(object):
         def old_intext_match(paragraph, ref):
             yr      = ref[1][2:]
             pattern = rf"({ref[0]}[^A-Za-z]+\'{yr})"
+            citations = re.findall(pattern, paragraph[1])
+            sentences = []
+            temp_i = re.split(r'\.\s(?=[A-Z])', paragraph[1])
+            for s in temp_i:
+                for cite in set(citations):
+                    if s.find(cite) != -1:
+                        sentences += [s]
+            return sentences
+        
+        # Create alternative function that will find citations even if authors
+        # year is misread by system
+        def alt_intext_match(paragraph, ref):
+            auth    = f'({ref[0]}'
+            pattern = auth + r"[^A-Za-z]+\([\W|^\d{4}]\))"
             citations = re.findall(pattern, paragraph[1])
             sentences = []
             temp_i = re.split(r'\.\s(?=[A-Z])', paragraph[1])
@@ -421,6 +472,11 @@ class pdf_scraper(object):
             for ref in self.intext_refs:
                 for paragraph in paras:
                     sentences += old_intext_match(paragraph, ref)
+        
+        for ref in self.intext_refs:
+            for paragraph in paras:
+                sentences += alt_intext_match(paragraph, ref)
+
         
         # Get sentences if citations are numbered or have a range
         if self.cite_nums:
@@ -521,9 +577,7 @@ class pdf_scraper(object):
                     output   += [s +'.']
             st.markdown(' '.join(output)) # Flatten ouput list and output
             
-        '''
-        - fix orphaning at reading of text, not at match
-        '''
+
 #------------------------------------------------------------------------------
 
 hover_text = 'Click Run to start the scraping'
